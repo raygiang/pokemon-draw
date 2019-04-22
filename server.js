4ada1c7cd80c21f329f90db91245260911dab9ea
Ray <raymondgiang@gmail.com>
2019-04-22 08:03:31 -0400

    initial commit

---
+++ server.js
@@ -0,0 +1,117 @@
+require('dotenv').config();
+
+const express = require('express');
+const bodyParser = require('body-parser');
+const fs = require('fs');
+
+// DeepAI
+const deepai = require('deepai');
+deepai.setApiKey(process.env.DEEPAI_KEY);
+
+// Mongo Atlas
+const MongoClient = require('mongodb').MongoClient;
+const uri = `mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PASSWORD}@testcluster-t9ymi.mongodb.net/test?retryWrites=true`;
+const client = new MongoClient(uri, { useNewUrlParser: true });
+let collection;
+
+const app = express();
+const port = process.env.PORT || 5000;
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+client.connect(err => {
+    collection = client.db("pokemonscores").collection("scores");
+});
+
+let compareScores = (res, pokeNum, dist, image) => {
+    let result;
+    let highScore;
+    let imagePath;
+
+    collection.findOne({ pokemonID: pokeNum })
+        .then(result => {
+            if(result) {
+                // New score, Update Database Entry and Overwrite saved Image
+                if(dist < result.score) {
+                    collection.updateOne({ pokemonID: pokeNum },
+                        { $set: { score: dist } }, (err, result) => {
+                            if (err) throw err;
+                        });
+                    fs.writeFile(`high-scores/${pokeNum}.png`, image, {encoding: 'base64'}, async (err) => {
+                        imagePath = fs.readFileSync(`high-scores/${pokeNum}.png`, { encoding: 'base64' });
+                        res.send({
+                            result: 'Congratulations, You have the high score!',
+                            distance: dist,
+                            highScore: dist,
+                            highScoreImage: imagePath
+                        });
+                    });
+                }
+                else {
+                    imagePath = fs.readFileSync(`high-scores/${pokeNum}.png`, { encoding: 'base64' });
+                    res.send({
+                        result: 'Unfortunately you did not get a high score.',
+                        distance: dist,
+                        highScore: result.score,
+                        highScoreImage: imagePath
+                    });
+                }
+            }
+            else {
+                let newScore = { pokemonID: pokeNum, score: dist };
+                collection.insertOne(newScore, (err, result) => {
+                    if (err) throw err;
+                });
+
+                fs.writeFile(`high-scores/${pokeNum}.png`, image, {encoding: 'base64'}, async (err) => {
+                    // console.log(fs.readFileSync(`high-scores/${pokeNum}.png`, { encoding: 'base64' }));
+                    imagePath = fs.readFileSync(`high-scores/${pokeNum}.png`, { encoding: 'base64' });
+                    res.send({
+                        result: 'Congratulations, You have the high score!',
+                        distance: dist,
+                        highScore: dist,
+                        highScoreImage: imagePath,
+                    });
+                });
+            }
+        })
+        .catch(err => {
+            console.log(err);
+        });
+};
+
+app.post('/saveImage', async (req, res) => {
+    let base64Image = req.body.image;
+
+    fs.writeFile("temp-drawings/test.png", base64Image, {encoding: 'base64'}, async (err) => {
+        console.log('File created');
+
+        let resp = await deepai.callStandardApi("image-similarity", {
+            image1: fs.createReadStream("temp-drawings/test.png"),
+            image2: req.body.graphImage,
+        });
+
+        let pokemonNumber = req.body.pokemonNumber;
+        let distance = resp.output.distance;
+
+        let result = compareScores(res, pokemonNumber, distance, base64Image);
+    });
+});
+
+process.on('SIGINT', function() {
+    client.close();
+});
+
+// https://medium.freecodecamp.org/how-to-make-create-react-app-work-with-a-node-backend-api-7c5c48acb1b0
+if (process.env.NODE_ENV === 'production') {
+    // Serve any static files
+    app.use(express.static(path.join(__dirname, 'client/build')));
+
+    // Handle React routing, return all requests to React app
+    app.get('*', function(req, res) {
+        res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
+    });
+}
+
+app.listen(port, () => console.log(`Listening on port ${port}`));
--- src/App.js
+++ src/App.js
@@ -1,25 +1,253 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import axios from 'axios';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+
+    // States
+    this.state = {
+      pokemonNumber: null,
+      pokemon: '',
+      sprite: '',
+      description: '',
+      pokeGraph: null,
+      compareResult:null,
+      distance: null,
+      highScore: null,
+      highScoreImage: null,
+    };
+
+    // Number of Pokemon
+    this.pokeCount = 802;
+
+    // Start and end position of cursor, used to draw lines on canvas
+    this.startPosition = {
+        xPos: 0,
+        yPos: 0
+    }
+    this.endPosition = {
+        xPos: 0,
+        yPos: 0
+    }
+
+    this.isDrawing = false;
+
+    // Get a random pokemon
+    this.getRandPokemon();
+
+    this.drawingURL = '';
+  }
+
+  // Get a random pokemon from PokeAPI
+  getRandPokemon = () => {
+    let pokedexNum = Math.floor(Math.random() * (this.pokeCount - 1)) + 1;
+    let pokeApiUrl = 'https://pokeapi.co/api/v2/';
+
+    // Get the name and sprite of the random pokemon
+    axios.get(pokeApiUrl + `pokemon/${pokedexNum}`)
+      .then(response => {
+        this.setState({
+          pokemonNumber: response.data.id,
+          pokemon: response.data.name.toUpperCase(),
+          sprite: response.data.sprites.front_default
+        })
+      });
+
+    // Get the description of the random pokemon
+    axios.get(pokeApiUrl + `pokemon-species/${pokedexNum}`)
+      .then(response => {
+        for(let i=0; i<response.data.flavor_text_entries.length; i++) {
+          if(response.data.flavor_text_entries[i].language.name === "en") {
+            this.setState({
+              description: response.data.flavor_text_entries[i].flavor_text
+            });
+            break;
+          }
+        }
+      });
+  };
+
+  // Get the graph version of pokemon from Wolfram Alpha API
+  getGraphPokemon = () => {
+    console.log(process.env.WOLFRAM_KEY);
+    let wolframApiUrl = `https://cors-anywhere.herokuapp.com/http://api.wolframalpha.com/v2/query?appid=${process.env.REACT_APP_WOLFRAM_KEY}&input=${this.state.pokemon}+like+curve&includepodid=PlotPod:PopularCurveData&podstate=PlotPod:PopularCurveData__Hide+axes&output=json`;
+    axios.get(wolframApiUrl)
+      .then(response => {
+        if(!response.data.queryresult.pods) {
+          this.getRandPokemon();
+        }
+        this.setState({ 
+          pokeGraph : response.data.queryresult.pods[0].subpods[0].img 
+        })
+      });
+  };
+
+  // Update the position of the cursor
+  updatePosition = (e, position, canvas) => {
+    let canvasBounds = canvas.getBoundingClientRect();
+
+    position.xPos = e.clientX - Math.floor(canvasBounds.left);
+    position.yPos = e.clientY - Math.floor(canvasBounds.top);
+  };
+
+  // Update the position of the cursor and draw a line from the start
+  // position to the end position
+  updateAndDraw = (e, drawingCanvas, context) => {
+    this.updatePosition(e, this.endPosition, drawingCanvas);
+    this.drawLine(context, this.startPosition.xPos, this.startPosition.yPos,
+      this.endPosition.xPos, this.endPosition.yPos, "#44429c");
+  };
+
+  // Function that runs while drawing on the canvas
+  drawing = (e, drawingCanvas, context) => {
+    if (this.isDrawing) {
+        this.updatePosition(e, this.startPosition, drawingCanvas);
+        setTimeout(this.updateAndDraw, 30, e, drawingCanvas, context);
+    }
+  };
+
+  /* Draw a line on the canvas
+     Parameters: context: context of the canvas
+                 startX: starting x position of the line
+                 startY: starting y position of the line
+                 endX: ending x position of the line
+                 endY: ending y position of the line
+                 colour: colour of the line
+  */
+  drawLine = (context, startX, startY, endX, endY, colour) => {
+    context.beginPath();
+    context.strokeStyle = colour;
+    context.lineWidth = 1.5;
+    context.moveTo(startX, startY);
+    context.lineTo(endX, endY);
+    context.stroke();
+    context.closePath();
+  };
+
+  // Send the base 64 image to the server and return the result from the server
+  saveAndCompareDrawing = async (base64Image) => {
+    const response = await fetch('/saveImage', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({
+        pokemonNumber: this.state.pokemonNumber,
+        image: base64Image,
+        graphImage: this.state.pokeGraph.src,
+      }),
+    });
+
+    const result = await response.json();
+
+    if (response.status !== 200) throw Error(result.message);
+
+    return result;
+  };
+
+  tryAgain = () => {
+    const drawingArea = document.getElementById("drawing-area");
+    drawingArea.style.display = "block";
+
+    this.setState({
+      compareResult: null,
+      distance: null,
+      highScore: null,
+      highScoreImage: null,
+    });
+  }
+
+  // On Component Mount
+  componentDidMount = () => {
+    const drawingArea = document.getElementById("drawing-area");
+    const drawingCanvas = document.getElementById("drawing-canvas");
+    const context = drawingCanvas.getContext("2d");
+    const compareButton = document.getElementById("compare-button");
+
+    // Add event listeners to the canvas
+    drawingCanvas.onmousedown = () => { this.isDrawing = true; };
+    drawingCanvas.onmousemove = (e) => { this.drawing(e, drawingCanvas, context); };
+    drawingCanvas.onmouseup = () => { this.isDrawing = false; };
+
+    // Get the image from the canvas in base64 and send it to the server
+    compareButton.onmouseup = async (e) => {
+      this.drawingURL = drawingCanvas.toDataURL('image/png');
+      let modifiedURL = this.drawingURL.replace('data:image/png;base64,', '');
+
+      drawingArea.style.display = "none";
+      this.saveAndCompareDrawing(modifiedURL)
+        .then(res => {
+          this.setState({
+            compareResult: res.result,
+            distance: res.distance,
+            highScore: res.highScore,
+            highScoreImage: 'data:image/gif;base64,' + res.highScoreImage,
+          });
+        })
+        .catch(err => console.log(err));
+    };
+  }
+
   render() {
+    const drawingCanvas = document.getElementById("drawing-canvas");
+
+    // If a random pokemon has been set but there is no graph yet, get the graph
+    if(this.state.pokeGraph === null && this.state.pokemon !== '' && this.state.description !== '') {
+      this.getGraphPokemon();
+      console.log("get graph");
+    }
+
+    // If the graph exists update the canvas width and height based on the graph
+    if(this.state.pokeGraph) {
+      drawingCanvas.width = this.state.pokeGraph.width;
+      drawingCanvas.height = this.state.pokeGraph.height;
+    }
+
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div className="page-wrapper">
+        <h1>Draw that Pokemon!?</h1>
+
+        {/* Info Area, Pokemon Name, Sprite, and Description */}
+        <div id="pokemon-info">
+          <h2>{this.state.pokemon}</h2>
+          <img src={this.state.sprite} alt={"Picture of " + this.state.pokemon} />
+          <h3>{this.state.description}</h3>
+        </div>
+
+        {/* Drawing Area, Pokemon Graph, Canvas, Compare Button*/}
+        <div id="drawing-area">
+          {
+            this.state.pokeGraph ?
+            <img src={this.state.pokeGraph ? this.state.pokeGraph.src : ''} alt="Graph of Pokemon" /> :
+            <div>Pokemon is Loading</div>
+          }
+          <canvas id="drawing-canvas"
+            style={{  
+              border: "1px solid #000", 
+              margin: "0 1em"
+            }}>
+          </canvas>
+          <button id="compare-button">Compare</button>
+        </div>
+
+        {/* If the distance is set then print (Closer to 0, the more similar) */}
+        {
+          this.state.distance ?
+          <div>
+            <div>Result: {this.state.distance}</div>
+            <div>Your Score: {this.state.distance}</div>
+            <div>High Score: {this.state.highScore}</div>
+            <div>Your Picture</div>
+            <img src={this.drawingURL} alt="Submitted Drawing" />
+            <div>Picture of High Score</div>
+            <img src={this.state.highScoreImage} alt="Drawing with current High Score" />
+            <button onClick={this.tryAgain}>Try Again</button>
+          </div>
+          :
+          ''
+        }
       </div>
     );
   }
