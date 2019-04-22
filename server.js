ad72c9c02fbf8972589e2bcab7539cb73a3b4c52
Ray <raymondgiang@gmail.com>
2019-04-22 10:17:35 -0400

    remove client reference from server.js

--- server.js
+++ server.js
@@ -107,11 +107,11 @@ process.on('SIGINT', function() {
 // https://medium.freecodecamp.org/how-to-make-create-react-app-work-with-a-node-backend-api-7c5c48acb1b0
 if (process.env.NODE_ENV === 'production') {
     // Serve any static files
-    app.use(express.static(path.join(__dirname, 'client/build')));
+    app.use(express.static(path.join(__dirname, 'build')));
 
     // Handle React routing, return all requests to React app
     app.get('*', function(req, res) {
-        res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
+        res.sendFile(path.join(__dirname, 'build', 'index.html'));
     });
 }
 
